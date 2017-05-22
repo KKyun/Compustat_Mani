47ba79a7c85e09adff214592b5a0c31ef3afb465
KKyun <k.kyun@protonmail.ch>
2017-05-22 17:45:25 -0400

    Update TopPlayerFinder.py
    Completed For Now

--- TopPlayerFinder.py
+++ TopPlayerFinder.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-result = []
+Result = []
 
 Filename = r".xlsx"
 
@@ -9,8 +9,15 @@ df = pd.read_excel(Filename)
 
 MinYear = df['Year'].min()
 MaxYear = df['Year'].max()
-for k in range(MinYear, MaxYear+1):
-  dfTemp = df[df['Year'] == k]
-  TotalPat = xlTemp['FILED'].count()
-  SemiPat = xlTemp['SemiPat'].sum()
-  EquiPat = xlTemp['EquiPat'].sum()
+for year in range(MinYear, MaxYear+1):
+    dfTemp = df[df['Year'] == year]
+    dfTemp['Rank'] = s['Revenue'].rank(ascending=False)
+    try:
+        Result.append({'Year':year, '1st':dfTemp[dfTemp['Rank'] == 1]['Company_Name'][0],
+                       '2nd':dfTemp[dfTemp['Rank'] == 2]['Company_Name'][0],
+                       '3rd':dfTemp[dfTemp['Rank'] == 3]['Company_Name'][0],
+                       '4th':dfTemp[dfTemp['Rank'] == 4]['Company_Name'][0],
+                       '5th':dfTemp[dfTemp['Rank'] == 5]['Company_Name'][0]})
+     
+df = pd.DataFrame(Result, columns=['Year','1st','2nd','3rd', '4th', '5th'])
+print(result)
